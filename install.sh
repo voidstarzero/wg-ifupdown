d273f704346667d07a2a825b6fc205a604607362
James Arcus <jimbo@ucc.asn.au>
2021-02-10 00:17:14 +0800

    Stop depending on sudo for install

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 # Remove old ifupdown wireguard scripts.
 pushd /etc/network
-sudo find -path './if-*' -name 'wireguard-*' -exec rm {} +
+find -path './if-*' -name 'wireguard-*' -exec rm {} +
 popd
 
 # Install ifupdown scripts to their proper locations.
 pushd ifupdown
-sudo cp -R * /etc/network/
+cp -R * /etc/network/
 popd
